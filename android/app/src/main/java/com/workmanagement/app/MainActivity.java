6e38f18bf8178f17b68b2ac150ea79af9b98357b
tomcat955 <Tombruzan@tcbmetalworks.com>
2025-12-21 16:41:35 -0600

    Fix MainActivity - Remove incorrect init() call, rely on @CapacitorPlugin auto-discovery

--- android/app/src/main/java/com/workmanagement/app/MainActivity.java
+++ android/app/src/main/java/com/workmanagement/app/MainActivity.java
@@ -1,19 +1,11 @@
 package com.workmanagement.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Manually register TextRecognitionPlugin
-        // This ensures the plugin is available even if auto-discovery fails
-        ArrayList<Class<? extends Plugin>> plugins = new ArrayList<>();
-        plugins.add(TextRecognitionPlugin.class);
-        this.init(savedInstanceState, plugins);
-    }
+    // TextRecognitionPlugin should be auto-discovered via @CapacitorPlugin annotation
+    // Make sure to:
+    // 1. Run "npx cap sync android" 
+    // 2. Clean and rebuild in Android Studio (Build > Clean Project, then Rebuild)
+    // 3. The @CapacitorPlugin annotation on TextRecognitionPlugin will handle registration
 }
